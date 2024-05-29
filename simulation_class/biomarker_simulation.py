4c67b8bfe20bc5acddeb03018aa3b50138406154
dsemchin <118320568+dsemchin@users.noreply.github.com>
2024-05-29 12:31:31 -0500

    +distro_fitter added into mcmc_analysis +added new plotting functions under biomarker_simulation +new docstrings +updated demo

--- simulation_class/biomarker_simulation.py
+++ simulation_class/biomarker_simulation.py
@@ -4,13 +4,26 @@ import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
 class BiomarkerSimulation:
-    def __init__(self, n_patients_stage, biomarkers_params, method='sigmoid_inv', add_noise=False, noise_std=0.1, random_state=None):
+    def __init__(self, n_patients_stage: dict, biomarkers_params: dict, method: str = 'sigmoid_inv', add_noise: bool = False, noise_std: float = 0.1, random_state: int = None, n_stages: int = 40):
+        """
+        Initializes the BiomarkerSimulation class.
+
+        Parameters:
+        n_patients_stage (dict): Dictionary specifying the number of patients at each disease stage.
+        biomarkers_params (dict): Dictionary specifying the parameters for each biomarker.
+        method (str): The simulation method to use ('sigmoid_inv', 'transition_matrix', 'ode').
+        add_noise (bool): Whether to add Gaussian noise to the biomarker values.
+        noise_std (float): The standard deviation of the Gaussian noise.
+        random_state (int): Seed for the random number generator for reproducibility.
+        n_stages (int): The number of stages to simulate.
+        """
         self.n_patients_stage = n_patients_stage
         self.biomarkers_params = biomarkers_params
         self.method = method
         self.add_noise = add_noise
         self.noise_std = noise_std
         self.random_state = random_state
+        self.n_stages = n_stages  # Number of stages for plotting
         self.X = None
         self.y = None
         self.t = None  # iime points for ODE
@@ -21,7 +34,7 @@ class BiomarkerSimulation:
         }
 
     def generate_patient(self, stage):
-        """delegates to the specific method based on the simulation type."""
+        """Delegates to the specific method based on the simulation type."""
         return self.methods[self.method](stage)
 
     def generate_patient_sigmoid(self, stage):
@@ -30,9 +43,6 @@ class BiomarkerSimulation:
 
         Parameters:
         stage (int): The disease stage of the patient.
-        add_noise (bool): If True, adds Gaussian noise to the biomarkers.
-        noise_std (float): The standard deviation of the Gaussian noise.
-        random_state (int): Seed for the random number generator for reproducibility.
 
         Returns:
         np.ndarray: An array of biomarker values for a single patient.
@@ -71,31 +81,25 @@ class BiomarkerSimulation:
         sol = solve_ivp(ode_system, t_span, y_init, t_eval=t_eval, method='RK45')
         return sol.t, sol.y.T
 
-    # def simulate(self):
-    #     if self.method in self.methods:
-    #         return self.methods[self.method]()
-    #     else:
-    #         raise ValueError(f"Method '{self.method}' not implemented.")
-
     def simulate(self):
         if self.method == 'ode':
             self.t, self.X = self.generate_patient_ode()
-            self.stages = np.zeros(self.X.shape[0])  # placeholder for stages,
-            threshold = 0.8  # threshold of disease
-            disease_biomarker_index = 0  # index of the biomarker used to determine health status
+            self.stages = np.zeros(self.X.shape[0])  # Placeholder for stages,
+            threshold = 0.8  # Threshold of disease
+            disease_biomarker_index = 0  # Index of the biomarker used to determine health status
             self.y = (self.X[:, disease_biomarker_index] > threshold).astype(int)
         else:
             X = []
             stages = []
-            for stage, total_number in self.n_patients_stage.items():
-                for _ in range(total_number):
+            for stage in range(self.n_stages):
+                for _ in range(self.n_patients_stage.get(stage, 0)):
                     patient_biomarkers = self.generate_patient(stage)
                     X.append(patient_biomarkers)
                     stages.append(stage)
             self.X = np.array(X)
             self.stages = np.array(stages)
-            n_healthy = sum(v for k, v in self.n_patients_stage.items() if k <= 3)
-            n_diseased = sum(self.n_patients_stage.values()) - n_healthy
+            n_healthy = sum(1 for stage in self.stages if stage <= 3)
+            n_diseased = len(self.stages) - n_healthy
             self.y = np.array([0] * n_healthy + [1] * n_diseased)
         return self.X, self.y, self.stages
 
@@ -103,27 +107,43 @@ class BiomarkerSimulation:
         """Plots the progression of biomarkers over stages."""
         if self.X is None or self.stages is None:
             raise ValueError("Simulation data not available. Run simulate() first.")
-        if self.method == 'ode':
-            plt.figure(figsize=(10, 5))
-            for i in range(self.X.shape[1]):
-                plt.plot(self.t, self.X[:, i], label=f'Biomarker {i+1}')
-            plt.xlabel('Time')
-            plt.ylabel('Biomarker Value')
-            plt.title("ODE Biomarker Progression Over Time")
-            plt.legend()
-            plt.show()
-        else:        
-            plt.figure(figsize=(12, 8))
-            num_biomarkers = self.X.shape[1]
-            unique_stages = np.unique(self.stages)
-            for i in range(num_biomarkers):
-                mean_values = [np.mean(self.X[self.stages == stage, i]) for stage in unique_stages]
-                std_values = [np.std(self.X[self.stages == stage, i]) for stage in unique_stages]
-                plt.errorbar(unique_stages, mean_values, yerr=std_values, fmt='-o', label=f'Biomarker {i+1}', capsize=5)
-            plt.xlabel('Patient Stage')
-            plt.ylabel('Biomarker Value')
-            plt.title(f'Biomarker Progression Using {self.method} Method')
-            plt.legend()
-            plt.grid(True)
-            plt.show()
         
+        plt.figure(figsize=(12, 8))
+        for i in range(self.X.shape[1]):
+            plt.plot(self.stages, self.X[:, i], label=f'Biomarker {i+1}')
+        plt.xlabel('Stage')
+        plt.ylabel('Biomarker Value')
+        plt.legend()
+        plt.show()
+    
+    def plot_patient_stages(self):
+        """Plots the progression of biomarkers with patient stages superimposed."""
+        if self.X is None or self.stages is None:
+            raise ValueError("Simulation data not available. Run simulate() first.")
+        
+        plt.figure(figsize=(12, 8))
+        for i in range(self.X.shape[1]):
+            plt.plot(self.stages, self.X[:, i], label=f'Biomarker {i+1}', alpha=0.3)
+        plt.scatter(self.stages, self.X[:, 0], label='Patient Stages', color='red', s=10)
+        plt.xlabel('Stage')
+        plt.ylabel('Biomarker Value')
+        plt.legend()
+        plt.show()
+    
+    def plot_biomarker_distribution(self, biomarker_index):
+        """Plots the distribution of a specific biomarker for healthy and unhealthy patients."""
+        if self.X is None or self.y is None:
+            raise ValueError("Simulation data not available. Run simulate() first.")
+        
+        biomarker_values = self.X[:, biomarker_index]
+        n_healthy = sum(self.y == 0)
+        n_diseased = sum(self.y == 1)
+        
+        bins = plt.hist(biomarker_values, alpha=0.1, label='all', bins=20)
+        plt.hist(biomarker_values[:n_healthy], label='healthy', bins=bins[1], alpha=0.3)
+        plt.hist(biomarker_values[n_healthy:], label='diseased', bins=bins[1], alpha=0.3)
+        plt.legend()
+        plt.xlabel('Biomarker Value')
+        plt.ylabel('Frequency')
+        plt.title(f'Distribution of Biomarker {biomarker_index + 1}')
+        plt.show()
