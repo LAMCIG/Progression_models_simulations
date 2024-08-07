53f200cf12bbbf23204e00b43f955e80db512ee1
Daniel Semchin <dsemchin@orpheus.iit.edu>
2024-08-07 10:45:35 -0500

    new ODE models added: +ACP model +Reaction Diffusion model ~TODO: fix normal diffusion model + Refactored 'ode' model to 'logistic' +priors added

--- simulation_class/ode_generator.py
+++ simulation_class/ode_generator.py
@@ -1,14 +1,26 @@
 import numpy as np
-
+from scipy.integrate import odeint
 class ODEGenerator:
-    def __init__(self, n_biomarkers, step=0.05, n_steps=200, neg_frac=0.01):
-        self.n_biomarkers = n_biomarkers
+    def __init__(self, n_biomarker_stages, model_type: str = 'logistic', step=0.05, n_steps=200, neg_frac=0.01):
+        self.n_biomarker_stages = n_biomarker_stages
         self.step = step
         self.n_steps = n_steps
         self.neg_frac = neg_frac
-        
-        # Attribute
+        self.model_type = model_type
         self.connectivity_matrix = None
+        self.time_points = np.linspace(0, 100, 1000)
+        
+    def generate_model(self):
+        if self.model_type == 'logistic':
+            return self.multi_logistic_sym_force()
+        elif self.model_type == 'acp':
+            return self.acp_model()
+        elif self.model_type == 'diffusion':
+            return self.diffusion_model()
+        elif self.model_type == 'reaction_diffusion':
+            return self.reaction_diffusion_model()
+        else:
+            raise ValueError(f"Unsupported model type: {self.model_type}")
 
     def random_connectivity_matrix(self, n, med_frac, source_rate, all_source_connections):
         A = np.random.rand(n, n)
@@ -46,14 +58,14 @@ class ODEGenerator:
         K[0, 1] = source_rate
         K[1, 0] = source_rate
 
-        self.random_connectivity_matrix = K
+        self.connectivity_matrix = K
         return K
-
+#%% LOGISTIC MODEL
     def multi_logistic_deriv_force(self, K, f, x):
         return np.dot(np.eye(K.shape[0]) - np.diag(x), np.dot(K, x) + f)
 
     def multi_logistic_sym_force(self):
-        n = self.n_biomarkers
+        n = self.n_biomarker_stages
         step = self.step
         n_steps = self.n_steps
         neg_frac = self.neg_frac
@@ -81,6 +93,79 @@ class ODEGenerator:
             x[:, i + 1] = x[:, i] + dx_dt * step
 
         return t, x
-    
+
+#%% ACP MODEL
+    def acp_model(self):
+        n = self.n_biomarker_stages
+        t = np.linspace(0, 100, 200)
+        x0 = np.zeros(n)
+        x0[0] = 0.05
+        
+        A = self.random_connectivity_matrix(n, 1.2, 0.1, False)
+        H = np.diag(np.sum(A, axis=0)) - A
+
+        k_ij = np.random.uniform(0.05, 1, n) 
+        gamma = np.random.uniform(0.4, 0.8, n)
+        eta = np.random.uniform(1.0, 1.5, n)
+        
+        print(f'k_ij: {k_ij}\ngamma: {gamma}\neta:{eta}')
+        
+        l1 = 3
+        l2 = 3
+
+        def acp_equations(f, tau, A, H, k_ij, gamma, eta, l1, l2):
+            K_ACP = k_ij / ((1 + np.exp(-l1 * (f - gamma))) * (1 + np.exp(l2 * (f - eta))))
+            R_ACP = k_ij / (1 + np.exp(l2 * (f - eta)))
+            dfdtau = np.dot((A * K_ACP), np.dot(H, f)) + R_ACP * f
+            return np.maximum(dfdtau, 0)
+        x = odeint(acp_equations, x0, t, args=(A, H, k_ij, gamma, eta, l1, l2))
+        return t, x.T
+
+#%% DIFFUSION MODEL
+    def diffusion_model(self):
+        n = self.n_biomarker_stages
+        t = np.linspace(0, 100, 200)
+        x0 = np.ones(n) * 0.5
+        x0[0] = 0.5
+            
+        A = self.random_connectivity_matrix(n, 1.2, 0.1, False)
+        H = np.diag(np.sum(A, axis=0)) - A
+
+        k_ij = np.random.uniform(0.05, 1, n)
+        K = np.diag(k_ij)
+
+        def diffusion_equations(f, tau, A, H, K):
+            dfdtau = -np.dot(np.dot(A * K, H), f)
+            return dfdtau
+            
+        x = odeint(diffusion_equations, x0, t, args=(A, H, K))
+        return t, x.T
+
+#%% REACTION DIFFUSION (RD)
+    def reaction_diffusion_model(self):
+        n = self.n_biomarker_stages
+        t = np.linspace(0, 100, 200)
+        x0 = np.ones(n) * 0.5
+        x0[0] = 0.5
+
+        A = self.random_connectivity_matrix(n, 1.2, 0.1, False)
+        H = np.diag(np.sum(A, axis=0)) - A
+
+        k_ij = np.random.uniform(0.2, 1, n)
+        K = np.diag(k_ij)
+        nu = np.random.uniform(1, 2, n) # maximal concentration threshold
+        R = np.random.uniform(0.1, 0.3, n) # agregation rate
+
+        def reaction_diffusion_equations(f, tau, A, H, K, R, nu):
+            diffusion_term = np.dot((A * K), np.dot(H, f))
+            reaction_term = R * f * (nu - f)
+            dfdtau = diffusion_term + reaction_term
+            return np.maximum(dfdtau, 0)
+
+        x = odeint(reaction_diffusion_equations, x0, t, args=(A, H, K, R, nu))
+        return t, x.T
+
+#%%
     def get_connectivity_matrix(self):
         return self.connectivity_matrix
+    
