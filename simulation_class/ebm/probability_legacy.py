5264dbb6383e5dd38d4981cd1d647431bde9d188
Boris Gutman <bgutman@orpheus.iit.edu>
2024-08-28 14:22:56 -0500

    Another attempt at direction-agnotsticity

--- simulation_class/ebm/probability_legacy.py
+++ simulation_class/ebm/probability_legacy.py
@@ -16,17 +16,30 @@ def fit_distributions(X, y, normalize=False):
     std = X[y==0, ...].std(axis=0)
     p_not_e = [norm(loc, s) for loc, s in zip(avg, std)]
 
-    # left_min = X.min(axis=0)
-    left_min = X[y==1, ...].min(axis=0)
+    left_min = X.min(axis=0)
+    # left_min = X[y==1, ...].min(axis=0)
     right_max = X[y==1, ...].max(axis=0)# avg.copy()
     
     p_e = [uniform(m1, m2-m1) for m1, m2 in zip(left_min, right_max)]
     return p_e, p_not_e, left_min, right_max
 
 
-def log_distributions_old(X, y, point_proba=False, *, X_test=None, y_test=None, normalize=False, eps=1e-8):
+def monotonize_X(X_,y):
+
+    vec = 1*( X_[y==1, ...].mean(axis=0) <  X_[y==0, ...].mean(axis=0))
+
+    X = X_.copy()
+    for i in np.shape(X_)[1]:
+        X[:,i] = X_[:,i]*vec
+
+    return X
+
+def log_distributions_old(X_, y, point_proba=False, *, X_test_=None, y_test=None, normalize=False, eps=1e-8):
     """Precomute probabilities for all features."""
     X = np.array(X).astype(np.float64)
+    X = monotonize_X(X_,y)
+    X_test = monotonize_X(X_test_,y)
+
     y = np.array(y)
     cdf_p_e, cdf_p_not_e, left_min, right_max = fit_distributions(X, y, normalize=normalize)
     
