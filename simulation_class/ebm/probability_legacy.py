0f28b427ed67feed757ca7a78347321dfa9915a5
Boris Gutman <bgutman@orpheus.iit.edu>
2024-08-28 15:03:39 -0500

    Fixed (hopefully) EBMAnalyzer transform fcn

--- simulation_class/ebm/probability_legacy.py
+++ simulation_class/ebm/probability_legacy.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import norm, uniform
 
 
 def fit_distributions(X_, y, normalize=False):
@@ -21,30 +22,65 @@ def fit_distributions(X_, y, normalize=False):
     right_max = X[y==1, ...].max(axis=0)# avg.copy()
     
     p_e = [uniform(m1, m2-m1) for m1, m2 in zip(left_min, right_max)]
-    return p_e, p_not_e, left_min, right_max
+    return p_e, p_not_e, left_min, right_max #, avg, std
 
 
-<<<<<<< HEAD
-=======
+# def compute_likelihoods_from_fitted_distributions(X_, y, normalize, left_min, right_max, avg, std):
+
+#     """
+#     Fit distribution p(x|E), p(x|~E) as a mixture of Gaussian and Uniform, see Fonteijn 
+#     section `Mixture models for the data likelihood`. 
+#     - P(x|E) = P(x > X | E)
+#     - P(x|~E) = P(x < X| ~E)
+#     """
+#     # # TODO: not sure about how to compute probabilities
+#     # from scipy.stats import norm, uniform
+#     if normalize:
+#         X = X / X.max(axis=1)[:, np.newaxis]
+    
+#     # avg = X[y==0, ...].mean(axis=0)
+#     # std = X[y==0, ...].std(axis=0)
+#     p_not_e = [norm(loc, s) for loc, s in zip(avg, std)]
+
+#     # left_min = X.min(axis=0)
+#     # # left_min = X[y==1, ...].min(axis=0)
+#     # right_max = X[y==1, ...].max(axis=0)# avg.copy()
+    
+#     p_e = [uniform(m1, m2-m1) for m1, m2 in zip(left_min, right_max)]
+#     return p_e, p_not_e
+
+
 def monotonize_X(X_,y):
 
-    vec = 2*(1*( X_[y==1, ...].mean(axis=0) <  X_[y==0, ...].mean(axis=0))  - 0.5)
+    flip_vec = 2*(1*( X_[y==1, ...].mean(axis=0) <  X_[y==0, ...].mean(axis=0))  - 0.5)
 
     X = X_.copy()
     for i in np.shape(X_)[1]:
-        X[:,i] = X_[:,i]*vec
+        X[:,i] = X_[:,i]*flip_vec
 
-    return X
+    return X, flip_vec
 
->>>>>>> b4dccb83894ca01fd3cf48792edcd7ab1aab591a
-def log_distributions_old(X_, y, point_proba=False, *, X_test_=None, y_test=None, normalize=False, eps=1e-8):
+def log_distributions(X_, y=None, 
+                      point_proba=False, *, X_test_=None, y_test=None, 
+                      normalize=False, eps=1e-8,
+                      fitted_cdfs = None):
     """Precomute probabilities for all features."""
     X = np.array(X).astype(np.float64)
     X = monotonize_X(X_,y)
-    X_test = monotonize_X(X_test_,y)
+    X_test, flip_vec = monotonize_X(X_test_,y)
 
-    y = np.array(y)
-    cdf_p_e, cdf_p_not_e, left_min, right_max = fit_distributions(X, y, normalize=normalize)
+    if (y is not None) & (fitted_cdfs is None):
+        y = np.array(y)
+        cdf_p_e, cdf_p_not_e, left_min, right_max = fit_distributions(X, y, normalize=normalize)
+    elif fitted_cdfs is not None:
+        cdf_p_e = fitted_cdfs[0]
+        cdf_p_not_e = fitted_cdfs[1]
+        left_min = fitted_cdfs[2]
+        right_max = fitted_cdfs[3]
+    else:
+        # except("throwing an error (no y or fitted params)")
+        print("throwing an error (no y or fitted params)")
+
     
     if X_test is not None:
         X = np.array(X_test).astype(np.float64)
@@ -74,7 +110,7 @@ def log_distributions_old(X_, y, point_proba=False, *, X_test_=None, y_test=None
                 p_left = cdf_p_not_e[j].cdf(X[i, j]-window/2)
                 p_right = cdf_p_not_e[j].cdf(X[i, j]+window/2)
                 log_p_not_e[i,j] = np.log(np.clip(p_right - p_left, eps, 1-eps))
-    return log_p_e, log_p_not_e
+    return log_p_e, log_p_not_e, cdf_p_e, cdf_p_not_e, left_min, right_max
 
 
 def predict_stage(event_order, log_p_e, log_p_not_e):
