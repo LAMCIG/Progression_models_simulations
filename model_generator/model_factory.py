57124154bda3385ae872476856348be577238130
Daniel Semchin <dsemchin@orpheus.iit.edu>
2024-10-10 12:30:32 -0500

    transition matrix + logistic ode reimplemeted

--- model_generator/model_factory.py
+++ model_generator/model_factory.py
@@ -1,5 +1,7 @@
-from model_generator.sigmoid_model import SigmoidModel
-from model_generator.acp_model import ACPModel
+from model_generator.other_models.sigmoid_model import SigmoidModel
+from model_generator.ode_models.acp_model import ACPModel
+from model_generator.ode_models.logistic_model import LogisticModel
+from model_generator.other_models.transition_matrix_model import TransitionMatrixModel
 class ModelFactory:
     
     @staticmethod
@@ -8,6 +10,10 @@ class ModelFactory:
             return SigmoidModel(**params)
         if model_type == 'acp':
             return ACPModel(**params)
+        if model_type == 'logistic':
+            return LogisticModel(**params)
+        if model_type == 'transition':
+            return TransitionMatrixModel(**params)
         # TODO: Add rest of the models
         else:
             raise ValueError(f"Unknown model type: {model_type}")
