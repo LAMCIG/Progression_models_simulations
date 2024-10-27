b0b8768c4e3bc7a4f75f782627e96b6f7da902ef
Daniel Semchin <dsemchin@orpheus.iit.edu>
2024-10-26 22:59:34 -0500

    finally got acp, rd, and diffusion working

--- scripts/run_ebm_logistic.sh
+++ scripts/run_ebm_logistic.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #PBS -S /bin/bash
 #PBS -o /home/$dsemchin/log -j y
-#PBS -l nodes=1:ppn=15  # Adjust the number of cores
-#PBS -l walltime=12:00:00  # Adjust wall time based on the expected duration
+#PBS -j oe 
+#PBS -l nodes=1:ppn=15
+#PBS -l walltime=12:00:00 
 
 cd /home/dsemchin/Progression_models_simulations/scripts
 
