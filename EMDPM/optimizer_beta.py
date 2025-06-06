f2ca1e3936d30fff058ee2b3fe445f227361df35
Daniel Semchin <dsemchin@orpheus.iit.edu>
2025-06-06 11:44:08 -0500

    EM rework semi-functional WIP

--- EMDPM/optimizer_beta.py
+++ EMDPM/optimizer_beta.py
@@ -56,7 +56,7 @@ def beta_loss_jac(beta_i: float, X_obs_i: np.ndarray, dt_i: np.ndarray,
     
     t_pred_i = dt_i + beta_i 
 
-    X_interp_i = np.array([
+    X_interp_i = np.stack([
         np.interp(t_pred_i, t_span, X_pred[i])
         for i in range(X_pred.shape[0])
     ])
@@ -68,14 +68,21 @@ def beta_loss_jac(beta_i: float, X_obs_i: np.ndarray, dt_i: np.ndarray,
 
     # dx/dt = (I - diag(x)) @ (scalar_K K x + f)
     I = np.eye(X_pred.shape[0])
-    #print("Eye terms: ", I.shape, X_pred.shape, np.diag(X_interp_i).shape)
+    
+    # print("Eye terms: ", I.shape, X_pred.shape, np.diag(X_interp_i).shape)
     
     dxdt_interp_i = np.zeros_like(X_interp_i)
     for j in range(X_interp_i.shape[1]):
         x_t = X_interp_i[:, j]
+        
+        assert x_t.ndim == 1 and x_t.shape[0] == n_biomarkers
+        assert np.issubdtype(x_t.dtype, np.floating)
+        #print("breakpoint 5: ", x_t.shape, (K @ x_t).shape, np.size((scalar_K * K @ x_t + f)), np.diag(x_t).shape, np.size(((np.eye(n_biomarkers) - np.diag(x_t)) @ (scalar_K * K @ x_t + f))))
+        y = (np.eye(n_biomarkers) - np.diag(x_t)) @ (scalar_K * K @ x_t + f)
+        print("y shape", y.shape, "y dtype", y.dtype, "y contents", y)
+        
         dxdt_interp_i[:, j] = (np.eye(n_biomarkers) - np.diag(x_t)) @ (scalar_K * K @ x_t + f)
         grad_reconstruction = -2 * np.sum(residuals * dxdt_interp_i)
-
     # print("cog params:", cog_i.shape, cog_a.shape, np.size(cog_b))
     cog_pred = cog_i @ cog_a + cog_b
     cog_prior = lambda_cog * np.sum((t_pred_i - cog_pred) ** 2)
@@ -85,45 +92,36 @@ def beta_loss_jac(beta_i: float, X_obs_i: np.ndarray, dt_i: np.ndarray,
     grad = grad_reconstruction + grad_cog
     return loss, grad
 
-def estimate_beta_for_patient(beta_i: float, df_patient: pd.DataFrame, x_reconstructed: np.ndarray,
-                               t_span: np.ndarray, t_max: float, use_jacobian: bool = False,
-                               lambda_cog: float = 0, a: float = 1, b: float = 0) -> float:
+def estimate_beta_for_patient(beta_i: float, X_obs_i: np.ndarray, dt_i: np.ndarray,
+                              X_pred: np.ndarray, t_span: np.ndarray,
+                              cog_i: np.ndarray, cog_a: np.ndarray, cog_b: float,
+                              theta: np.ndarray, K: np.ndarray, lambda_cog: float = 0.0,
+                              use_jacobian: bool = False, t_max: float = 12.0) -> float:
     """
     Estimates the optimal beta_i for a single patient.
 
     Parameters
     ----------
-    df_patient : pd.DataFrame
-        Subset of data for a single patient.
-    x_reconstructed : np.ndarray
-        Current model prediction of biomarker trajectories.
-    t_span : np.ndarray
-        Time points used in model simulation.
-    t_max : float
-        Maximum allowed time shift.
-
+    # TODO: DOCSTRINGS
     Returns
     -------
     float
         Optimized beta_i value for the patient.
     """
-    
-    dt_obs = df_patient["dt"].values
-    x_obs = df_patient[[col for col in df_patient.columns if "biomarker_" in col]].values.T
-
     #beta_guess = np.median(dt_obs)
     beta_guess = beta_i
-    s_ij = df_patient["cognitive_score"].values # np.ndarray of cog scores
     
     if use_jacobian == True:
         loss_function = beta_loss_jac
+        args=(X_obs_i, dt_i, X_pred, t_span, cog_i, cog_a, cog_b, theta, K, lambda_cog)
     else:
         loss_function = beta_loss
+        args=(X_obs_i, dt_i, X_pred, t_span, cog_i, cog_a, cog_b, lambda_cog)
 
     result = minimize(
         loss_function,
         x0=beta_guess,
-        args=(dt_obs, x_obs, x_reconstructed, t_span, lambda_cog, s_ij, a, b),
+        args=args,
         jac=use_jacobian,
         bounds=[(0, t_max)],
         method="L-BFGS-B"
